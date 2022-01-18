2e635f122e59736830d318bc8fefae6ad1cdf439
Cong Chanh Vinzenz Nguyen <congchanh@outlook.de>
2022-01-18 17:14:15 +0100

    Projektdateien hinzufügen.

---
+++ ObserverPatternExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPatternExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomValueProvider randomValueProvider = new RandomValueProvider();
+            RandomValueObserver randomValueObserver1 = new RandomValueObserver();
+            RandomValueObserver randomValueObserver2 = new RandomValueObserver();
+
+            randomValueProvider.Subscribe(randomValueObserver1);
+            randomValueProvider.Subscribe(randomValueObserver2);
+
+            randomValueProvider.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ObserverPatternExample/RandomValueProvider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObserverPatternExample
+{
+    public class RandomValueProvider : RandomValueObserver
+    {
+        IList<IObserver<int>> _observers=new List<IObserver<int>>();
+        bool shoudStop=true;
+        Random rnd= new Random();
+
+        public IDisposable Subscribe(RandomValueObserver observer)
+        {
+            _observers.Add(observer);
+            return observer;
+        }
+
+        public void Start()
+        {
+            shoudStop = false;
+            Task.Factory.StartNew(createValues);
+        }
+
+        void createValues()
+        {
+    
+            while (shoudStop==false)
+            {
+                var nextValue = rnd.Next();
+                Notify(nextValue);
+                Thread.Sleep(500);
+            }
+        }
+
+        public void Notify(int nextValue)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(nextValue);
+            }
+        }
+
+        public void Stop()
+        {
+            shoudStop=true;
+        }
+    }
+
+    public class RandomValueObserver : IObserver<int>, IDisposable
+    {
+        private static int counter=0;
+        public RandomValueObserver()
+        {
+            this.Id = counter;
+            counter++;
+        }
+
+        int Id { get; set; }
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(int value)
+        {
+            Console.WriteLine("Observer "+Id+" : " +value.ToString());
+        }
+    }
+}
